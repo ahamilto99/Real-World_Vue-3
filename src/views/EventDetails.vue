ebb54bcb809e0cabde85460075832e8d78f4c9e3
ahamilto99 <ajhamilton15@hotmail.com>
2022-01-29 10:57:14 -0500

    Completed lesson 6

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null
+    }
+  },
+  async created() {
+    try {
+      const response = await EventService.getEvent(this.$route.params.id)
+      this.event = response.data
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
